1c710937bd49172a4a8d7de3a5087b1fec73d0ee
PeixotoLucass <lucpeixoto@uolinc.com>
2022-09-02 16:11:50 -0300

    Adicionado os arquivos ao projeto no GitHub

---
+++ src/main/java/com/projetonetflix/projetonetflix/ProjetoNetflixApplication.java
@@ -0,0 +1,13 @@
+package com.projetonetflix.projetonetflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoNetflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoNetflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetonetflix/projetonetflix/ProjetoNetflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetonetflix.projetonetflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoNetflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
