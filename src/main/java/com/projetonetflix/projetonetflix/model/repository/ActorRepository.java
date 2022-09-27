1a5cf5e199e0f048c8d23bd219e9059d5b3c6f11
PeixotoLucass <lucpeixoto@uolinc.com>
2022-09-27 14:36:53 -0300

    BUS-4763 Criação repository

---
+++ src/main/java/com/projetonetflix/projetonetflix/model/repository/ActorRepository.java
@@ -0,0 +1,7 @@
+package com.projetonetflix.projetonetflix.model.repository;
+
+import com.projetonetflix.projetonetflix.model.entity.Actor;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ActorRepository extends CrudRepository<Actor, Integer> {
+}
