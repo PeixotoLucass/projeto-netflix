5a1c0bb8abb33fd2a5c8192702720baafea6825a
PeixotoLucass <lucpeixoto@uolinc.com>
2022-10-05 14:00:42 -0300

    BUS-4823 atualização dos imports no ActorController

--- src/main/java/com/projetonetflix/projetonetflix/controller/ActorController.java
+++ src/main/java/com/projetonetflix/projetonetflix/controller/ActorController.java
@@ -4,10 +4,12 @@ import com.projetonetflix.projetonetflix.dto.ActorDTO;
 import com.projetonetflix.projetonetflix.model.entity.Actor;
 import com.projetonetflix.projetonetflix.service.ActorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/netflix/actor")
